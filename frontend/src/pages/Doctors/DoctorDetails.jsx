2ba3744450dd18d82e636b08284b10cb5cfdba07
samarthbluelupin <145488981+samcoder07@users.noreply.github.com>
2024-04-12 21:20:48 +0530

    login authentication ui completed

--- frontend/src/pages/Doctors/DoctorDetails.jsx
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -3,6 +3,7 @@ import doctorImg from "../../assets/images/doctor-img02.png";
 import starIcon from "../../assets/images/Star.png"
 import DoctorAbout from './DoctorAbout';
 import Feedback from './Feedback'
+import SidePanel from './SidePanel';
 const DoctorDetails = () => {
 
 	const [tab, setTab] = useState('about')
@@ -55,7 +56,9 @@ const DoctorDetails = () => {
 							}
 						</div>
 					</div>
-					<div></div>
+					<div>
+						<SidePanel />
+					</div>
 				</div>
 			</div>
 		</section>
