4bb5f59802a7bdc45e3f138955799d5d6d9ffdc4
samarthbluelupin <145488981+samcoder07@users.noreply.github.com>
2024-05-10 16:18:21 +0530

    Update SidePanel.jsx

--- frontend/src/pages/Doctors/SidePanel.jsx
+++ frontend/src/pages/Doctors/SidePanel.jsx
@@ -1,7 +1,7 @@
 import convertTime from '../../utils/convertTime'
 const SidePanel = ({ ticketPrice, timeSlots, doctorId }) => {
 	return (
-		<div className='shadow-panelShadow p-3 lg:p-5 rounded-md'>
+		<div className='shadow-lg p-3 lg:p-5 rounded-md'>
 			<div className="flex items-center justify-between">
 				<p className='text__para mt-0 font-semibold'>Ticket Price</p>
 				<span className='text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor font-bold'>
